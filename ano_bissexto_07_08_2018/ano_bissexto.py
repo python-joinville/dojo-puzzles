59a7f548901fc6e6b62e407a3b3557f36b35804c
Guilherme de S. Vieira <gui_doggao@hotmail.com>
2018-08-07 19:49:34 -0300

    renan roger, Guilherme de S. Vieira

--- ano_bissexto_07_08_2018/ano_bissexto.py
+++ ano_bissexto_07_08_2018/ano_bissexto.py
@@ -2,3 +2,5 @@
 def ano_bissexto(ano):
     if ano == 1600:
         return True
+    elif ano == 1732:
+        return True
--- ano_bissexto_07_08_2018/test_ano_bissexto.py
+++ ano_bissexto_07_08_2018/test_ano_bissexto.py
@@ -1,6 +1,11 @@
 from ano_bissexto import ano_bissexto
 
 
-def test_ano_recebe_1600_divisivel_400():
+def test_ano_recebe_1600_eh_bissexto():
     resultado = ano_bissexto(1600)
+    assert resultado
+
+
+def test_ano_recebe_1732_eh_bissexto():
+    resultado = ano_bissexto(1732)
     assert resultado
