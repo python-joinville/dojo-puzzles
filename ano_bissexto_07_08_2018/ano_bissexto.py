a4019603f4ee2260d600fcbfdb89d1c8123e9b4e
Guilherme de S. Vieira <gui_doggao@hotmail.com>
2018-08-07 20:12:26 -0300

    Murilo O Nascimento, Ronier P Gutierrez

--- ano_bissexto_07_08_2018/ano_bissexto.py
+++ ano_bissexto_07_08_2018/ano_bissexto.py
@@ -4,5 +4,7 @@ def ano_bissexto(ano):
         return True
     elif ano == 1732:
         return True
-    elif ano%400 == 0:
+    elif ano % 400 == 0:
         return True
+    else:
+        return False
--- ano_bissexto_07_08_2018/test_ano_bissexto.py
+++ ano_bissexto_07_08_2018/test_ano_bissexto.py
@@ -10,7 +10,18 @@ def test_ano_recebe_1732_eh_bissexto():
     resultado = ano_bissexto(1732)
     assert resultado
 
+
+def test_ano_recebe_1742_eh_bissexto():
+    resultado = ano_bissexto(1742)
+    assert resultado == False
+
+
 def test_ano_400():
     for i in range(10):
         resultado = ano_bissexto(i*400)
         assert resultado
+
+
+def test_ano_negativo():
+    resultado = ano_bissexto(-400)
+    assert resultado
