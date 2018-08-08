028a096280a3d088deed54613959c17c002d6fc7
Guilherme de S. Vieira <gui_doggao@hotmail.com>
2018-08-07 21:17:33 -0300

    Murilo O Nascimento, Gabriela Moreira

--- ano_bissexto_07_08_2018/ano_bissexto.py
+++ ano_bissexto_07_08_2018/ano_bissexto.py
@@ -2,9 +2,9 @@
 
 def ano_bissexto(ano):
     if type(ano) is int and ano >= 0:
-        return bool(eh_divisivel(ano, 400) or (
-                        eh_divisivel(ano, 4) and
-                        not eh_divisivel(ano, 100))))
+        return bool((eh_divisivel(ano, 4) and
+                        not eh_divisivel(ano, 100)) or
+                        eh_divisivel(ano, 400))
     return False
 
 
