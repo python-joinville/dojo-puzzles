a34635ec9346def60c8feb902929d71d5d100917
GuilhermeVBeira <gui_doggao@hotmail.com>
2018-11-01 21:13:28 -0300

    Piloto: Guilherme de Souza Vieira, co-piloto: Bernardelli

---
+++ palavras_primas_01_11_2018/palavra_prima.py
@@ -0,0 +1,8 @@
+def palavra_prima(palavra):
+
+    if palavra == 'a':
+        return True
+    if palavra == 'ab':
+        return True
+
+    return False
---
+++ palavras_primas_01_11_2018/test_palavra.py
@@ -0,0 +1,13 @@
+from palavra_prima import palavra_prima
+
+def test_palavra_a_retorna_true():
+    esperado = palavra_prima('a')
+    assert esperado
+
+def test_palavra_ab_retorna_true():
+    esperado = palavra_prima('ab')
+    assert esperado
+
+def test_palabra_abc_retorna_false():
+    esperado = palavra_prima('abc')
+    assert not esperado
