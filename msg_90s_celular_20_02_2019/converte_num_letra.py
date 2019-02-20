bb22eb48999e48b8aaedd57a838479770110bef5
Guilhermevbeira <gui_doggao@hotmail.com>
2019-02-20 20:51:24 -0300

    Piloto: Guilherme de S Vieira, Co-piloto: Nathália

--- msg_90s_celular_20_02_2019/converte_num_letra.py
+++ msg_90s_celular_20_02_2019/converte_num_letra.py
@@ -2,7 +2,13 @@ tabela = {'2':'a',
           '3':'d', 
           '5':'j',
           '4':'g',
-          '6':'m'}
+          '6':'m',
+          '7':'p',
+          '8':'t',
+          '9': 'w',
+          '0': ' ',
+
+}
 
 def converte_num_letra(digito):
     return tabela[digito]
--- msg_90s_celular_20_02_2019/test_qualquercoisa.py
+++ msg_90s_celular_20_02_2019/test_qualquercoisa.py
@@ -2,27 +2,17 @@ from converte_num_letra import converte_num_letra
 import pytest
 
 @pytest.mark.parametrize("digito,expected", [
-    ("2", 'a'),
-    
+    ('2', 'a'),
+    ('3', 'd'),
+    ('4', 'g'),
+    ('5', 'j'),
+    ('6', 'm'),
+    ('7', 'p'),
+    ('8', 't'),    
+    ('9', 'w'),
+    ('0', ' '),
 ])
-def test_eval(digito, expected):
+def test_converte_num_digito(digito, expected):
     resultado = converte_num_letra(digito)
     assert resultado == expected
-
-def test_imprime_a():
-    resultado = converte_num_letra('2')
-    assert resultado == 'a'
-
-def test_imprime_d():
-    resultado = converte_num_letra('3')
-    assert resultado == 'd'
-
-def test_imprime_j():
-    assert converte_num_letra('5') == 'j'
-
-def test_imprime_g():
-    assert converte_num_letra('4') == 'g'
-
-def test_imprime_m():
-    assert converte_num_letra('6') == 'm'
 
