c5f0a04c097cd10bf820ffa525a1a3255661119c
Guilhermevbeira <gui_doggao@hotmail.com>
2019-02-20 21:20:05 -0300

    Piloto: Tiago, Co-piloto: Bellon

--- msg_90s_celular_20_02_2019/converte_num_letra.py
+++ msg_90s_celular_20_02_2019/converte_num_letra.py
@@ -1,24 +1,21 @@
-tabela = {'2': ['a', 'b', 'c']
-        #   '3':'d', 
-        #   '5':'j',
-        #   '4':'g',
-        #   '6':'m',
-        #   '7':'p',
-        #   '8':'t',
-        #   '9': 'w',
-        #   '0': ' ',
+tabela = {'2': 'a',
+          '3':'d', 
+          '5':'j',
+          '4':'g',
+          '6':'m',
+          '7':'p',
+          '8':'t',
+          '9': 'w',
+          '0': ' ',
 }
 
 def converte_num_letra(digitos):
     palavra = ''
     letra = ''
-    ocorrencia = 0
+    sequencia = ''
     for digito in digitos:
-        if ocorrencia == 0:
-            letra = digito
-            palavra = tabela[digito][ocorencia] 
-        elif digito == letra:
-            palavra = tabela[digito][ocorencia]  
-        # palavra = palavra + tabela[digito]
-        ocorencia =+ 1
+        if letra != '' and digito != letra:
+            palavra = palavra + tabela[digito]        
+        sequencia += digito             
+        palavra = palavra + tabela[digito]
     return palavra
